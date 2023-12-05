0671a431178190120fd824f4fa73eca20fe1c1c7
Roman <roman@mgstr.ru>
2023-12-05 14:42:46 +0600

    fix: remove sample code

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="">
       <div>GraphiQL App</div>
     </main>
   );
