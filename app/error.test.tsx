330f9f570b62eca970adbc4a5ca68d5ca948a631
Roman <roman@mgstr.ru>
2023-12-07 12:11:08 +0600

    feat: add error boundary, add tests, exlude layout.tsx from coverage

---
+++ app/error.test.tsx
@@ -0,0 +1,43 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Error from "./error";
+
+describe("Error page", () => {
+  jest.mock(
+    "next/link",
+    () =>
+      ({ children }: { children: HTMLElement }) =>
+        children
+  );
+
+  it("renders content", () => {
+    render(
+      <Error
+        error={{ digest: "string", name: "name", message: "message" }}
+        reset={function (): void {}}
+      />
+    );
+
+    const errorHeader = screen.getByText("It looks like the app is broken...");
+    const toMainBtn = screen.getByText("Main Page");
+    const againBtn = screen.getByText("Try again");
+    const notFoundImg = screen.getByAltText("error");
+
+    expect(errorHeader).toBeInTheDocument();
+    expect(toMainBtn).toBeInTheDocument();
+    expect(againBtn).toBeInTheDocument();
+    expect(notFoundImg).toBeInTheDocument();
+  });
+
+  it("Go-To-Main button should links to main", async () => {
+    render(
+      <Error
+        error={{ digest: "string", name: "name", message: "message" }}
+        reset={function (): void {}}
+      />
+    );
+    const toMainBtn = screen.getByText("Main Page");
+
+    expect(toMainBtn.closest("a")).toHaveAttribute("href", "/");
+  });
+});
