4e3766d48b8ca2d2d38c809087014b82f639032e
Roman <roman@mgstr.ru>
2023-12-08 14:56:41 +0600

    test: add test for lang btns

--- app/components/header.test.tsx
+++ app/components/header.test.tsx
@@ -1,21 +1,66 @@
-import { render, screen, waitFor } from "@testing-library/react";
+import {
+  act,
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+} from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { Header } from "./header";
+import RootLayout from "../layout";
 
 describe("Header", () => {
-  it("renders content", () => {
-    render(<Header />);
+  jest.mock("./footer", () => {
+    return {
+      Footer: jest.fn().mockReturnValue(<div>Test Footer</div>),
+    };
+  });
+  // eslint-disable-next-line no-console
+  console.error = jest.fn();
 
+  beforeEach(() => {
+    render(
+      <RootLayout>
+        <div>Children</div>
+      </RootLayout>
+    );
+  });
+  it("should take the default lang from Context", () => {
+    const enBtn = screen.getByText("En");
+    const ruBtn = screen.getByText("Ru");
+
+    expect((enBtn as HTMLButtonElement).disabled).toEqual(true);
+    expect((ruBtn as HTMLButtonElement).disabled).toEqual(false);
+  });
+
+  it("should change lang on btns click", () => {
+    const ruBtn = screen.getByText("Ru");
+    act(() => {
+      fireEvent.click(ruBtn);
+    });
+    expect((ruBtn as HTMLButtonElement).disabled).toEqual(true);
+  });
+});
+
+describe("Header", () => {
+  beforeEach(() => {
+    render(<Header />);
+  });
+
+  it("renders content", () => {
+    const langEn = screen.getByText("En");
+    const langRu = screen.getByText("Ru");
     const mainIcon = screen.getByTitle("welcome page");
     const exitIcon = screen.getByTitle("sign out");
     const signInIcon = screen.getByTitle("sign in");
 
+    expect(langEn).toBeInTheDocument();
+    expect(langRu).toBeInTheDocument();
     expect(mainIcon).toBeInTheDocument();
     expect(exitIcon).toBeInTheDocument();
     expect(signInIcon).toBeInTheDocument();
   });
   it("changes styles if to have scroll", async () => {
-    render(<Header />);
     const scrollEvent = new Event("scroll");
     const header = screen.getByTestId("header-test");
 
