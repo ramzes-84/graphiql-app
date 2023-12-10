7764eb6005c150836fe0fd86dc22f3194883ed1a
Lyubov Agulova <lu7623@gmail.com>
2023-12-10 13:33:09 +0300

    feat: add firebase adapter

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { FirestoreAdapter } from "@auth/firebase-adapter";
+import { cert } from "firebase-admin/app";
+import { Adapter } from "next-auth/adapters";
 
 export const authOptions = {
   providers: [
@@ -8,6 +11,13 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
+  adapter: FirestoreAdapter({
+    credential: cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, "\n"),
+    }),
+  }) as Adapter,
 };
 
 export const handler = NextAuth(authOptions);
--- app/main/page.tsx
+++ app/main/page.tsx
@@ -4,15 +4,23 @@ import { USUAL_BTN } from "../styles/uni-classes";
 import { useDict } from "../utils/useDictHook";
 import { signOut, useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
+import { db } from "../firebase";
+import { collection, query } from "firebase/firestore";
+import { useCollection } from "react-firebase-hooks/firestore";
 
 function Page() {
   const dict = useDict();
   const { data: session, status } = useSession();
+  const [users] = useCollection(query(collection(db, "users")));
 
   if (status !== "authenticated") redirect("/");
   return (
     <>
       <div>{dict.mainPage}</div>
+      <ul>
+        {users &&
+          users.docs.map((x) => <li key={x.data().name}>{x.data().email}</li>)}
+      </ul>
       <div>{session?.user?.name}</div>
       <button
         className={USUAL_BTN}
