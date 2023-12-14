bc2ee0b28cfd4d01d30c56ebf3ebe1f8521de1c5
Lyubov Agulova <lu7623@gmail.com>
2023-12-14 10:59:38 +0300

    fix: build fix

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,22 +1,26 @@
 import NextAuth from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "@/firebase";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions = {
   pages: {
     signIn: "/login",
   },
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
+    /* eslint-disable-next-line @typescript-eslint/ban-ts-comment*/
+    // @ts-ignore <-- according to https://github.com/nextauthjs/next-auth/issues/2701
     CredentialsProvider({
-      name: "credentials",
       credentials: {
-        email: { label: "Email", type: "text", placeholder: "john@gmail.com" },
-        password: { label: "Password", type: "password" },
+        email: { label: "email", type: "email", required: true },
+        password: { label: "password", type: "password", required: true },
       },
       /* eslint-disable-next-line @typescript-eslint/ban-ts-comment*/
-      // @ts-ignore <-- this is like an official solution for a problem with types, as next-auth is being developed with strict: false, ugh. Reference: https://github.com/nextauthjs/next-auth/issues/2701
+      // @ts-ignore <-- according to https://github.com/nextauthjs/next-auth/issues/2701
       async authorize(credentials) {
+        if (!credentials?.email || !credentials.password) return null;
+
         return await signInWithEmailAndPassword(
           auth,
           credentials ? credentials.email : "",
