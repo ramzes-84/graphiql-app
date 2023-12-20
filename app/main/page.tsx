32daab51d390139ec6032d0e1bcdc2c608269213
Kseniya <pen.cil@mail.ru>
2023-12-20 17:13:43 +0500

    fix: fix comments

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -10,14 +10,11 @@ const Page = () => {
   const dict = useDict();
 
   const [response, setResponse] = useState<IResponse>({});
-  const getResponse = (response: IResponse) => {
-    setResponse(response);
-  };
   return (
     <>
       <div className={H1}>{dict.mainPage}</div>
       <div className="flex m-3 p-3 gap-5 h-screen bg-fuchsia-50 rounded">
-        <Editor callback={getResponse} />
+        <Editor callback={(resp) => setResponse(resp)} />
         <Viewer response={response} />
       </div>
     </>
