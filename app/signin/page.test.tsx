22bef019d7fb0085d07630c92c61d40e42af5190
Lyubov Agulova <lu7623@gmail.com>
2023-12-21 13:07:00 +0300

    fix: fix coverage

--- app/signin/page.test.tsx
+++ app/signin/page.test.tsx
@@ -6,13 +6,17 @@ export const mockSession = {
   data: null,
   expires: new Date(Date.now() + 2 * 86400).toISOString(),
 };
-const resp = { error: "Firebase: Error (auth/invalid-credential)." };
+const resp1 = { error: "Firebase: Error (auth/network-request-failed)." };
+const resp2 = { error: "Firebase: Error (auth/invalid-credential)." };
 
 jest.mock("next-auth/react", () => ({
   useSession: jest.fn(() => {
     return { data: mockSession, status: "unauthenticated" };
   }),
-  signIn: jest.fn().mockImplementation(() => Promise.resolve(resp)),
+  signIn: jest
+    .fn()
+    .mockImplementationOnce(() => Promise.resolve(resp2))
+    .mockImplementation(() => Promise.resolve(resp1)),
 }));
 describe("Page", () => {
   beforeEach(() => {
@@ -49,6 +53,17 @@ describe("Page", () => {
       expect(
         screen.getByText("Invalid e-mail address or password")
       ).toBeInTheDocument();
+    });
+  });
+  it("Show network error", async () => {
+    const email = screen.getByRole("textbox", { name: /email/i });
+    const password = screen.getByRole("textbox", { name: /password/i });
+    const btn = screen.getByText("Sign In");
+    fireEvent.change(email, { target: { value: "111@test.com" } });
+    fireEvent.change(password, { target: { value: "Pass111)" } });
+    await waitFor(() => {
+      fireEvent.click(btn);
+      expect(screen.getByText("Network failed")).toBeInTheDocument();
     });
   });
 });
