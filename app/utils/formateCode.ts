07f64fbb9207ab87afc8cb3153e8ad32f90171a3
Kseniya <pen.cil@mail.ru>
2023-12-22 01:45:58 +0500

    fix: some fix in formatecode function

--- app/utils/formateCode.ts
+++ app/utils/formateCode.ts
@@ -1,28 +1,41 @@
 export const formatCode = (text: string) => {
-  let result = "";
-  result = text.replace(/\{/g, " {\n ");
-  result = result.replace(/\s*;\s*/g, "; ");
-  result = result.replace(/,\s*/g, ", ");
-  result = result.replace(/:\s*/g, ": ");
-  result = result.replace(/\s*\)\s*/g, ") ");
-  result = result.replace(/\s*}\s*/g, "\n}");
+  const arrStrings = text.split("\n");
 
-  const lines = result.split("\n");
+  if (arrStrings.length === 0) return;
+
+  const newArr: string[] = [];
+  arrStrings.forEach((item) => {
+    if (item === "" || item.trim().startsWith("#")) {
+      newArr.push(item);
+    } else {
+      let result = item.trim();
+      result = result.replace(/\{/g, " {\n");
+      result = result.replace(/,\s*/g, ", ");
+      result = result.replace(/:\s*/g, ": ");
+      result = result.replace(/\s*\)\s*/g, ") ");
+      result = result.replace(/\s*}\s*/g, "\n}");
+      newArr.push(result);
+    }
+  });
+
   let stringLevel = 0;
   let formattedCode = "";
 
-  for (let i = 0; i < lines.length; i += 1) {
-    if (lines[i].includes("{")) {
-      stringLevel += 1;
-    }
-    for (let j = 0; j < stringLevel; j += 1) {
-      formattedCode += " ";
-    }
-    formattedCode += lines[i] + "\n";
-    if (lines[i].includes("}")) {
-      stringLevel -= 1;
+  for (let i = 0; i < newArr.length; i += 1) {
+    if (newArr[i] === "" || newArr[i].trim().startsWith("#")) {
+      formattedCode += newArr[i] + "\n";
+    } else {
+      if (newArr[i].includes("{")) {
+        stringLevel += 1;
+      }
+      for (let j = 0; j < stringLevel; j += 1) {
+        formattedCode += " ";
+      }
+      formattedCode += newArr[i] + "\n";
+      if (newArr[i].includes("}")) {
+        stringLevel -= 1;
+      }
     }
   }
-
   return formattedCode;
 };
