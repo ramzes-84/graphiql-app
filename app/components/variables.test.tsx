6de45c7b14edf3974723254a59a1c29bc199d157
Lyubov Agulova <lu7623@gmail.com>
2024-01-01 22:02:49 +0300

    fix: minor tests fix

--- app/components/variables.test.tsx
+++ app/components/variables.test.tsx
@@ -1,12 +1,9 @@
 import { render, screen, waitFor } from "@testing-library/react";
 import { Variables } from "./variables";
 
-const mockCallback = jest.fn();
 describe("Variables", () => {
   it("renders variables section", async () => {
-    render(
-      <Variables callback={(text) => mockCallback(text)} format={false} />
-    );
+    render(<Variables />);
     await waitFor(async () => {
       const variables = screen.getByTitle("variables");
       const textbox = screen.getByRole("textbox");
